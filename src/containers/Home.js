6f493e4126014896af5405e7a63fbb577a85d61f
raptoracademy <raptoracademy.in@gmail.com>
2021-06-12 23:13:40 +0530

    Setting up our React app

---
+++ src/containers/Home.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./Home.css";
+
+export default function Home() {
+  return (
+    <div className="Home">
+      <div className="lander">
+        <h1>A New Way to Learn</h1>
+        <p className="text-muted">
+          Raptor Academy is the best platform to help you enhance your skills,
+          expand your knowledge and prepare for engineering and medical exams.
+        </p>
+      </div>
+    </div>
+  );
+}
